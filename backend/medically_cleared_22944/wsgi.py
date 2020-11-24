f9c16983b5b072dd8272071120c369028c49f071
Crowdbotics <team@crowdbotics.com>
2020-11-24 03:30:58 +0000

    Update models

--- backend/medically_cleared_22944/wsgi.py
+++ backend/medically_cleared_22944/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medically_cleared_22944.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "medically_cleared_22944.settings")
 
 application = get_wsgi_application()
